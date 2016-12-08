a42306fda7f1c8824835540e479cee95095e2b9b
Ana Maria Sosa <anamariasosam@gmail.com>
2016-12-08 14:47:15 -0500

    Add reducer books file

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Captain Ahab'},
+    { title: 'Albus Dumbledore'},
+    { title: 'Dr Alexandre Manette'},
+    { title: ' Art is Dead'},
+  ]
+}
